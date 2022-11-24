9d9a88a061250a1345376435b29395181a85028c
VasillevArtem <94571976+VasillevArtem@users.noreply.github.com>
2022-11-24 18:40:18 -0500

    Add RndString class and ReturnRndString method.

---
+++ CSharpFinalProject/RndString.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFinalProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFinalProject
+{
+    static internal class RndString
+    {                 
+        static public string ReturnRndString(Random rand, int maxValue)
+        {
+            char[] chars = new char[rand.Next(1, maxValue)];
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = (char)rand.Next(0x0410, 0x44F);
+            }
+            return new string(chars);
+        }
+    }
+    
+
+}
+
+
